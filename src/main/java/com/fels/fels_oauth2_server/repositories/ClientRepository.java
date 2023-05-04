0449f61007fabb8c258e947de7e1ebe9f52e6495
NjengaFelix <felixnjunge78@gmail.com>
2023-05-04 13:42:40 +0300

    Added optional findByClientId

--- src/main/java/com/fels/fels_oauth2_server/repositories/ClientRepository.java
+++ src/main/java/com/fels/fels_oauth2_server/repositories/ClientRepository.java
@@ -3,5 +3,8 @@ package com.fels.fels_oauth2_server.repositories;
 import com.fels.fels_oauth2_server.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client,Long> {
+    Optional<Client> findByClientId(String clientId);
 }
