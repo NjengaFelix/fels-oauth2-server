48832c0df1c8761ca834571d5a8d2369a889d978
NjengaFelix <felixnjunge@rocketmail.com>
2023-06-07 10:46:54 +0300

    removed password algorithm

--- src/test/java/com/fels/fels_oauth2_server/services/CustomUserDetailsServiceTest.java
+++ src/test/java/com/fels/fels_oauth2_server/services/CustomUserDetailsServiceTest.java
@@ -1,11 +1,9 @@
 package com.fels.fels_oauth2_server.services;
 
 import com.fels.fels_oauth2_server.entities.Authority;
-import com.fels.fels_oauth2_server.entities.PasswordAlgorithm;
 import com.fels.fels_oauth2_server.entities.User;
 import com.fels.fels_oauth2_server.entities.UserDetail;
 import com.fels.fels_oauth2_server.repositories.AuthorityRepository;
-import com.fels.fels_oauth2_server.repositories.PasswordAlgorithmRepository;
 import com.fels.fels_oauth2_server.repositories.UserDetailRepository;
 import com.fels.fels_oauth2_server.repositories.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,15 +14,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.mockito.Mockito.verify;
 import static org.assertj.core.api.Assertions.assertThat;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class CustomUserDetailsServiceTest {
-    @Mock private PasswordAlgorithmRepository passwordAlgorithmRepository;
     @Mock private AuthorityRepository authorityRepository;
     @Mock private UserRepository userRepository;
     @Mock private UserDetailRepository userDetailRepository;
@@ -34,27 +26,8 @@ class CustomUserDetailsServiceTest {
     void setUp() {
         customUserDetailsService = new CustomUserDetailsService(
                 userDetailRepository,userRepository,
-                authorityRepository, passwordAlgorithmRepository
+                authorityRepository
         );
-    }
-
-    @Test
-    void savePasswordAlgorithm() {
-        //given
-        PasswordAlgorithm passwordAlgorithm = new PasswordAlgorithm(
-                "bcrypt","bcrypt algorithm identifier"
-        );
-        //when
-        customUserDetailsService.savePasswordAlgorithm(passwordAlgorithm);
-        //then
-        ArgumentCaptor<PasswordAlgorithm> passwordAlgorithmArgumentCaptor =
-                ArgumentCaptor.forClass(PasswordAlgorithm.class);
-
-        verify(passwordAlgorithmRepository).save(passwordAlgorithmArgumentCaptor.capture());
-
-        PasswordAlgorithm capturedPasswordAlgorithm = passwordAlgorithmArgumentCaptor.getValue();
-
-        assertThat(capturedPasswordAlgorithm).isEqualTo(passwordAlgorithm);
     }
 
     @Test
@@ -77,18 +50,13 @@ class CustomUserDetailsServiceTest {
     @Test
     void saveUserDetail() {
         //given
-        PasswordAlgorithm passwordAlgorithm = new PasswordAlgorithm(
-                "bcrypt","bcrypt algorithm identifier"
-        );
-        customUserDetailsService.savePasswordAlgorithm(passwordAlgorithm);
         Authority authority = new Authority(
                 "read","Read authority - Can only read content"
         );
         authorityRepository.save(authority);
         User user = new User(
                 "","","","",
-                "","","felixnjunge78@gmail.com",
-                passwordAlgorithm,"$12345"
+                "","","felixnjunge78@gmail.com","$12345"
         );
         userRepository.save(user);
         //when
@@ -109,14 +77,9 @@ class CustomUserDetailsServiceTest {
     @Test
     void saveUser() {
         //given
-        PasswordAlgorithm passwordAlgorithm = new PasswordAlgorithm(
-                "bcrypt","bcrypt algorithm identifier"
-        );
-        customUserDetailsService.savePasswordAlgorithm(passwordAlgorithm);
         User user = new User(
                 "","","","",
-                "","","felixnjunge78@gmail.com",
-                passwordAlgorithm,"$12345"
+                "","","felixnjunge78@gmail.com","$12345"
         );
         //when
         userRepository.save(user);
