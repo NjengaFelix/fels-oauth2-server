3bf2add99b7f6de558422f65e0465d685425c65f
NjengaFelix <felixnjunge@rocketmail.com>
2023-06-07 10:46:23 +0300

    removed PasswordAlgorithm

--- src/test/java/com/fels/fels_oauth2_server/repositories/UserDetailRepositoryTest.java
+++ src/test/java/com/fels/fels_oauth2_server/repositories/UserDetailRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.fels.fels_oauth2_server.repositories;
 
 import com.fels.fels_oauth2_server.entities.Authority;
-import com.fels.fels_oauth2_server.entities.PasswordAlgorithm;
 import com.fels.fels_oauth2_server.entities.User;
 import com.fels.fels_oauth2_server.entities.UserDetail;
 import org.junit.jupiter.api.Test;
@@ -9,15 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
-import java.util.function.Predicate;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class UserDetailRepositoryTest {
-    @Autowired
-    private PasswordAlgorithmRepository passwordAlgorithmRepository;
     @Autowired
     private UserRepository userRepository;
     @Autowired
@@ -28,14 +23,9 @@ class UserDetailRepositoryTest {
     @Test
     void findByUsername() {
         //given
-        PasswordAlgorithm passwordAlgorithm = new PasswordAlgorithm(
-          "bcrypt","bcrypt algorithm identifier"
-        );
-        passwordAlgorithmRepository.save(passwordAlgorithm);
         User user = new User(
                 "","","","",
-                "","","felixnjunge78@gmail.com",
-                passwordAlgorithm,"$12345"
+                "","","felixnjunge78@gmail.com","$12345"
         );
         userRepository.save(user);
         Authority authority = new Authority(
--- src/test/java/com/fels/fels_oauth2_server/repositories/UserRepositoryTest.java
+++ src/test/java/com/fels/fels_oauth2_server/repositories/UserRepositoryTest.java
@@ -1,36 +1,24 @@
 package com.fels.fels_oauth2_server.repositories;
 
-import com.fels.fels_oauth2_server.entities.PasswordAlgorithm;
 import com.fels.fels_oauth2_server.entities.User;
-import com.fels.fels_oauth2_server.entities.UserDetail;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
 import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    private PasswordAlgorithmRepository passwordAlgorithmRepository;
 
     @Test
     void findByUsername() {
         //given
-        PasswordAlgorithm passwordAlgorithm = new PasswordAlgorithm(
-                "bcrypt","bcrypt algorithm identifier"
-        );
-        passwordAlgorithmRepository.save(passwordAlgorithm);
-
         User user = new User(
                 "","","","",
-                "","","felixnjunge78@gmail.com",
-                passwordAlgorithm,"$12345"
+                "","","felixnjunge78@gmail.com","$12345"
         );
         userRepository.save(user);
         //when
