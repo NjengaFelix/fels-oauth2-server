5973dfac5cd17c7cf423cdad57a03cad0516661f
NjengaFelix <felixnjunge@rocketmail.com>
2023-06-07 10:47:04 +0300

    removed password algorithm

--- src/main/java/com/fels/fels_oauth2_server/services/CustomUserDetailsService.java
+++ src/main/java/com/fels/fels_oauth2_server/services/CustomUserDetailsService.java
@@ -1,25 +1,26 @@
 package com.fels.fels_oauth2_server.services;
 
 import com.fels.fels_oauth2_server.entities.Authority;
-import com.fels.fels_oauth2_server.entities.PasswordAlgorithm;
 import com.fels.fels_oauth2_server.entities.User;
 import com.fels.fels_oauth2_server.entities.UserDetail;
 import com.fels.fels_oauth2_server.models.SecurityUser;
 import com.fels.fels_oauth2_server.repositories.AuthorityRepository;
-import com.fels.fels_oauth2_server.repositories.PasswordAlgorithmRepository;
 import com.fels.fels_oauth2_server.repositories.UserDetailRepository;
 import com.fels.fels_oauth2_server.repositories.UserRepository;
 import jakarta.transaction.Transactional;
-import org.aspectj.bridge.MessageUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -27,19 +28,12 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserDetailRepository userDetailRepository;
     private final UserRepository userRepository;
     private final AuthorityRepository authorityRepository;
-    private final PasswordAlgorithmRepository passwordAlgorithmRepository;
     private Logger logger = LoggerFactory.getLogger(CustomUserDetailsService.class);
 
-    public CustomUserDetailsService(UserDetailRepository userDetailRepository, UserRepository userRepository, AuthorityRepository authorityRepository, PasswordAlgorithmRepository passwordAlgorithmRepository) {
+    public CustomUserDetailsService(UserDetailRepository userDetailRepository, UserRepository userRepository, AuthorityRepository authorityRepository) {
         this.userDetailRepository = userDetailRepository;
         this.userRepository = userRepository;
         this.authorityRepository = authorityRepository;
-        this.passwordAlgorithmRepository = passwordAlgorithmRepository;
-    }
-
-    //Save passwordAlgorithm
-    public PasswordAlgorithm savePasswordAlgorithm(PasswordAlgorithm passwordAlgorithm) {
-        return passwordAlgorithmRepository.save(passwordAlgorithm);
     }
 
     //Save authority
@@ -62,10 +56,22 @@ public class CustomUserDetailsService implements UserDetailsService {
         Optional<User> optionalUser = userRepository.findByUsername(username);
         try {
             User user = optionalUser.get();
-            logger.info(user.getUserDetails().toString());
+            logger.debug(user.getUserDetails().toString());
             return new SecurityUser(user);
         } catch (UsernameNotFoundException e) {
             throw new UsernameNotFoundException("User name not found");
         }
+    }
+
+    public List<GrantedAuthority> getUserAuthorities(String username) {
+        User user = userRepository.findByUsername(username).orElse(null);
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        if (user != null) {
+            authorities = user.getUserDetails()
+                    .stream()
+                    .map(a -> new SimpleGrantedAuthority(a.getAuthority().getName()))
+                    .collect(Collectors.toList());
+        }
+        return authorities;
     }
 }
